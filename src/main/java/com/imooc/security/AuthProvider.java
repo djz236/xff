974ea3182cd5f209918d100ebcf063e34a524730
djz236 <djz236@163.com>
2019-03-31 14:19:46 +0800

    5

--- src/main/java/com/imooc/security/AuthProvider.java
+++ src/main/java/com/imooc/security/AuthProvider.java
@@ -1,3 +1,28 @@
+/*********************************************************
+ *********************************************************
+ ********************                  *******************
+ *************                                ************
+ *******                  _oo0oo_                  *******
+ ***                     o8888888o                     ***
+ *                       88" . "88                       *
+ *                       (| -_- |)                       *
+ *                       0\  =  /0                       *
+ *                     ___/`---'\___                     *
+ *                   .' \\|     |// '.                   *
+ *                  / \\|||  :  |||// \                  *
+ *                 / _||||| -:- |||||- \                 *
+ *                |   | \\\  -  /// |   |                *
+ *                | \_|  ''\---/''  |_/ |                *
+ *                \  .-\__  '-'  ___/-. /                *
+ *              ___'. .'  /--.--\  `. .'___              *
+ *           ."" '<  `.___\_<|>_/___.' >' "".            *
+ *          | | :  `- \`.;`\ _ /`;.`/ - ` : | |          *
+ *          \  \ `_.   \_ __\ /__ _/   .-` /  /          *
+ *      =====`-.____`.___ \_____/___.-`___.-'=====       *
+ *                        `=---='                        *
+ *      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~      *
+ *********__佛祖保佑__永无BUG__验收通过__钞票多多__*********
+ *********************************************************/
 package com.imooc.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,36 +38,65 @@ import com.imooc.entity.User;
 import com.imooc.service.IUserService;
 
 /**
- * 自定义认证实现
- * Created by 瓦力.
+ * @ClassName: AuthProvider
+ * @Description:TODO(这里用一句话描述这个类的作用)
+ * @author: 公司名称
+ * @date: 2019年3月15日 下午9:34:19
+ * 
+ * @Copyright: 2019 www.xxx.com Inc. All rights reserved.
+ *             注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他的商业目
  */
 public class AuthProvider implements AuthenticationProvider {
-    @Autowired
-    private IUserService userService;
 
-    private final Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
+	@Autowired
+	private IUserService userService;
+private final Md5PasswordEncoder passwordEncoder=new Md5PasswordEncoder();
+	/**
+	 * <p>
+	 * Title: authenticate
+	 * </p>
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @param authentication
+	 * @return
+	 * @throws AuthenticationException
+	 * @see org.springframework.security.authentication.AuthenticationProvider#authenticate(org.springframework.security.core.Authentication)
+	 */
+	@Override
+	public Authentication authenticate(Authentication authentication)
+			throws AuthenticationException {
+		String userName = authentication.getName();
+		String inputPassword = (String) authentication.getCredentials();
+		User user = userService.findUserByName(userName);
+		if (user == null) {
+			throw new AuthenticationCredentialsNotFoundException("authError");
+		}
+		//user.getPassword()
+		if(passwordEncoder.isPasswordValid(user.getPassword(),inputPassword, user.getId()))
+			{
+			return new UsernamePasswordAuthenticationToken(user,null, user.getAuthorities());
+			}
+		throw new BadCredentialsException("authError");
+	}
 
-    @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String userName = authentication.getName();
-        String inputPassword = (String) authentication.getCredentials();
+	/**
+	 * <p>
+	 * Title: supports
+	 * </p>
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @param authentication
+	 * @return
+	 * @see org.springframework.security.authentication.AuthenticationProvider#supports(java.lang.Class)
+	 */
+	@Override
+	public boolean supports(Class<?> authentication) {
+//		return false;
+		return true;//支持所有的认证类
+	}
 
-        User user = userService.findUserByName(userName);
-        if (user == null) {
-            throw new AuthenticationCredentialsNotFoundException("authError");
-        }
-
-        if (this.passwordEncoder.isPasswordValid(user.getPassword(), inputPassword, user.getId())) {
-            return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-
-        }
-
-        throw new BadCredentialsException("authError");
-
-    }
-
-    @Override
-    public boolean supports(Class<?> authentication) {
-        return true;
-    }
 }
